0f627f21ecd4393d35b55ec85bd97fa26d67985a
Jarvis <kamaleshselvam75@gmail.com>
2025-07-06 15:47:22 +0530

    frontend over

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,10 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
+import ShortenUrlPage from './pages/ShortenUrl';
+import ErrorPage from './pages/ErrorPage';
 import { Toaster } from 'react-hot-toast';
+import DashboardLayout from "./components/Dashboard/DashboardLayout";
 const  App=()=>{
 
   return (
@@ -15,10 +18,15 @@ const  App=()=>{
         <Navbar/>
         <Toaster position='bottom-center'/>
         <Routes>
-          <Route path='/' element={<Hero />}></Route>
-          <Route path='/about' element={<About />}></Route>
-          <Route path='/signup' element={<Signup/>}></Route>
-          <Route path='/signin' element={<Signin/>}></Route>
+          <Route path='/' element={<Hero />}/>
+          <Route path='/about' element={<About />}/>
+           <Route path="/s/:url" element={<ShortenUrlPage />} />
+          <Route path='/signup' element={<Signup/>}/>
+          <Route path='/signin' element={<Signin/>}/>
+           <Route path='/dashboard' element={<DashboardLayout/>}/>
+           <Route path="/error" element={<ErrorPage/>}/>
+            <Route path="*" element={ <ErrorPage message="We can't seem to find the page you're looking for"/>} />
+
         </Routes>
         <Footer/>
       </Router>
